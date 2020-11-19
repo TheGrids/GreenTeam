8afa17f1122c4bd226e3e844d6496fd09167f2a4
TheGrids <72039184+TheGrids@users.noreply.github.com>
2020-11-19 21:15:10 +0300

    Убирание лишнего, готовый дизайн activity_main

--- app/src/main/java/com/greenteam/voller2/MainActivity.kt
+++ app/src/main/java/com/greenteam/voller2/MainActivity.kt
@@ -1,16 +1,54 @@
 package com.greenteam.voller2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
-    val field_of_activity = arrayOf("Помощь детям с ограниченными возможностями здоровья","Помощь взрослым с ограниченными возможностями здоровья","Помощь многодетным семьям","Экологические инициативы","Помощь бездомным животным","Помощь пожилым людям и ветеранам", "Организация патриотической направленности","Охрана здоровья, поддержка здорового образа жизни","Поисково-спасательное движение","Культурные инициативы","Поддержка семьи, материнство, отцовства, детства","Защита прав и свобод человека")
+    val field_of_activity = arrayOf("Помощь детям с ограниченными возможностями здоровья", "Помощь взрослым с ограниченными возможностями здоровья", "Помощь многодетным семьям", "Экологические инициативы", "Помощь бездомным животным", "Помощь пожилым людям и ветеранам", "Организация патриотической направленности", "Охрана здоровья, поддержка здорового образа жизни", "Поисково-спасательное движение", "Культурные инициативы", "Поддержка семьи, материнство, отцовства, детства", "Защита прав и свобод человека")
 
+    lateinit var option : Spinner
+    lateinit var result : TextView
+   // lateinit var test :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //option = findViewById(R.id.sp_option) as Spinner
+
+        //option.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, field_of_activity)
+
+
+
     }
+
+    fun toastMe(view: View){
+        val myToast = Toast.makeText(this, "Hello, Toast!", Toast.LENGTH_SHORT)
+
+        myToast.show()
+    }
+
+    fun loginin(view: View){
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    //    fun test(view: View) {
+    //        setContentView(R.layout.test)
+    //    }
+//    fun countMe(view: View){
+//        val textView: TextView = findViewById(R.id.ad)
+//        var count: Int = Integer.parseInt(textView.text.toString())
+//        count++
+//
+//        textView.text = count.toString()
+//    }
 }
---
+++ app/src/main/java/com/greenteam/voller2/reg_org.kt
@@ -0,0 +1,11 @@
+package com.greenteam.voller2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reg_org : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg_org)
+    }
+}
